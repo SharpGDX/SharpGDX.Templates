06527cf1908f89db48186b5d0c77fd3ae46c92de
Richard Preziosi <17318983+RichardPreziosi@users.noreply.github.com>
2024-11-30 13:28:42 -0500

    Initial templates commit.

---
+++ src/SharpGDX.Templates/content/ClassLibrary/Main.cs
@@ -0,0 +1,30 @@
+﻿using SharpGDX;
+
+namespace Desktop;
+
+internal class Main : IApplicationListener
+{
+    public void Create()
+    {
+    }
+
+    public void Dispose()
+    {
+    }
+
+    public void Pause()
+    {
+    }
+
+    public void Render()
+    {
+    }
+
+    public void Resize(int width, int height)
+    {
+    }
+
+    public void Resume()
+    {
+    }
+}
---
+++ src/SharpGDX.Templates/content/Desktop/DesktopLauncher.cs
@@ -0,0 +1,45 @@
+﻿using SharpGDX.Desktop;
+
+namespace Desktop
+{
+    /// <summary>
+    ///     Launches the desktop application.
+    /// </summary>
+    internal class DesktopLauncher
+    {
+        public static void Main(string[] args)
+        {
+            CreateApplication();
+        }
+
+        private static void CreateApplication()
+        {
+            _ = new DesktopApplication(new Main(), GetDefaultConfiguration());
+        }
+
+        private static DesktopApplicationConfiguration GetDefaultConfiguration()
+        {
+            var configuration = new DesktopApplicationConfiguration();
+
+            configuration.SetTitle("YourProjectName");
+
+            // Vsync limits the frames per second to what your hardware can display, and helps eliminate
+            // screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
+            configuration.UseVsync(true);
+
+            // Limits FPS to the refresh rate of the currently active monitor, plus 1 to try to match fractional
+            // refresh rates. The Vsync setting above should limit the actual FPS to match the monitor.
+            configuration.SetForegroundFPS(DesktopApplicationConfiguration.GetDisplayMode().RefreshRate + 1);
+
+            // If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
+            // useful for testing performance, but can also be very stressful to some hardware.
+            // You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
+            configuration.SetWindowedMode(640, 480);
+
+            // You can change these files; they are in Desktop/src/main/resources/ .
+            configuration.SetWindowIcon("SharpGDX128.png", "SharpGDX64.png", "SharpGDX32.png", "SharpGDX16.png");
+
+            return configuration;
+        }
+    }
+}
